7e0660be5d2ee1e4735278ef42f4fbd87525ed66
cherr000 <chudue@hotmail.com>
2017-02-03 17:35:37 -0600

    update some code and layout

--- dicegame.js
+++ dicegame.js
@@ -13,27 +13,33 @@ function gameStarter() {
 	gamePlay();
 }
 function gameRule() {
-	return alert ("First one to get even number win the game, if tie replay.");
+	alert ("First one to get even number win the game, if tie reply.");
 }
 function selectionDice() {
 	var diceOption = prompt ("Selection Dice Choice. 4,6,8,10,12 or 20");
 	switch(diceOption) {
 		case ("4"):
+		document.getElementById("Dice").innerHTML = "Dice selected:"+ diceOption;
 		console.log(diceOption);
 		return diceOption;
 		case ("6"):
+		document.getElementById("Dice").innerHTML = "Dice selected:"+ diceOption;
 		console.log(diceOption);
 		return diceOption;
 		case ("8"):
+		document.getElementById("Dice").innerHTML = "Dice selected:"+ diceOption;
 		console.log(diceOption);
 		return diceOption;
 		case ("10"):
+		document.getElementById("Dice").innerHTML = "Dice selected:"+ diceOption;
 		console.log(diceOption);
 		return diceOption;
 		case ("12"):
+		document.getElementById("Dice").innerHTML = "Dice selected:"+ diceOption;
 		console.log(diceOption);
 		return diceOption;
 		case ("20"):
+		document.getElementById("Dice").innerHTML = "Dice selected:"+ diceOption;
 		console.log(diceOption);
 		return diceOption;
 		default:
@@ -50,27 +56,27 @@ function diceSide() {
 }
 function cpuDice() {
     var min = 1;
-    var max = 10;
+    var max = 20;
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 function gamePlay() {
 	var playerOne = diceSide();
 	var playerTwo = cpuDice();
 	var player = 0;
-	console.log("Player 1 Roll: " + playerOne, "Player 2 Roll:" + playerTwo);
+	document.getElementById("diceRoll1").innerHTML = "Player 1 Roll: " + playerOne;
+	document.getElementById("diceRoll2").innerHTML = "Player 2 Roll: " + playerTwo;
 	if (playerOne %2 == 0 && playerTwo %2 != 0){
 		alert ("playerOne Win");
-		console.log("playerOne Win")
+		document.getElementById("playerOneWin").innerHTML = "playerOne Win. ";
 	}
 	else if (playerTwo %2 == 0 && playerOne %2 != 0){
-		alert ("playerTwo Win");
-		console.log("playerTwo Win")
+		alert ("AI Win");
+		document.getElementById("playerTwoWin").innerHTML = "AI Win. ";
 	}
 	else if ((playerOne%2===0)&& (playerTwo%2===0)||(playerOne%1===0)&& (playerTwo%1===0)) {
-		alert("Tie!");
+		alert("Tie! Play Again");
+		document.getElementById("Tie").innerHTML = "Both Player Tie. ";
 		gamePlay()
 	}
 	return player;
 }
-var test = gameStarter();
-console.log(test);
