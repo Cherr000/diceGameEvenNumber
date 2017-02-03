937d8c0c3a9a6cbea72a3677aa20693a597fa33b
cherr000 <chudue@hotmail.com>
2017-02-03 10:31:51 -0600

    fix bug

--- dicegame.js
+++ dicegame.js
@@ -4,8 +4,8 @@
 // game play - done
 // make player 2 own random box - done
 // make game to work - done
-// make prompt only work for selected number
-// make loop for tie
+// make loop for tie - done
+// make prompt only work for selected number - done
 
 "use strict"
 function gameStarter() {
@@ -16,9 +16,32 @@ function gameRule() {
 	return alert ("First one to get even number win the game, if tie reply.");
 }
 function selectionDice() {
-	var dicePicker = prompt ("Selection Dice Choice. 4,6,8,10,12 or 20");
-	console.log("Dice Choice: " + dicePicker);
-	return dicePicker;
+	var diceOption = prompt ("Selection Dice Choice. 4,6,8,10,12 or 20");
+	switch(diceOption) {
+		case ("4"):
+		console.log(diceOption);
+		return diceOption;
+		case ("6"):
+		console.log(diceOption);
+		return diceOption;
+		case ("8"):
+		console.log(diceOption);
+		return diceOption;
+		case ("10"):
+		console.log(diceOption);
+		return diceOption;
+		case ("12"):
+		console.log(diceOption);
+		return diceOption;
+		case ("20"):
+		console.log(diceOption);
+		return diceOption;
+		default:
+		alert ("Not a Option Repick");
+		selectionDice();
+		break;
+	}
+	return diceOption;
 }
 function diceSide() {
     var min = 1;
@@ -34,16 +57,19 @@ function gamePlay() {
 	var playerOne = diceSide();
 	var playerTwo = cpuDice();
 	var player = 0;
+	console.log("Player 1 Roll: " + playerOne, "Player 2 Roll:" + playerTwo);
 	if (playerOne %2 == 0 && playerTwo %2 != 0){
 		alert ("playerOne Win");
+		console.log("playerOne Win")
 	}
 	else if (playerTwo %2 == 0 && playerOne %2 != 0){
 		alert ("playerTwo Win");
+		console.log("playerTwo Win")
 	}
 	else if ((playerOne%2===0)&& (playerTwo%2===0)||(playerOne%1===0)&& (playerTwo%1===0)) {
 		alert("Tie!");
+		gamePlay()
 	}
-	console.log("Player 1 Roll: " + playerOne, "Player 2 Roll:" + playerTwo);
 	return player;
 }
 var test = gameStarter();
